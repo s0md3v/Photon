beae3f9cda7c730e4e5eeecc8b588d69c8cffda0
Somdev Sangwan <s0md3v@gmail.com>
2018-07-25 00:54:26 +0530

    Create dnsmap.py

---
+++ plugins/dnsmap.py
@@ -0,0 +1,15 @@
+import re
+import requests
+
+def dnsdumpster(domain):
+	response = requests.Session().get('https://dnsdumpster.com/').text
+	csrf_token = re.search(r"name='csrfmiddlewaretoken' value='(.*?)'", response).group(1)
+
+	cookies = {'csrftoken': csrf_token}
+	headers = {'Referer': 'https://dnsdumpster.com/'}
+	data = {'csrfmiddlewaretoken': csrf_token, 'targetip': domain}
+	response = requests.Session().post('https://dnsdumpster.com/', cookies=cookies, data=data, headers=headers)
+
+	image = requests.get('https://dnsdumpster.com/static/map/%s.png' % domain)
+	with open('%s.png' % domain, 'wb') as f:
+	    f.write(image.content)
