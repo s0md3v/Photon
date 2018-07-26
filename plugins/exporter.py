68afb657cbb39e336b20312b59886dcd0b64c0e1
Somdev Sangwan <s0md3v@gmail.com>
2018-07-26 13:45:20 +0530

    added directory argument

--- plugins/exporter.py
+++ plugins/exporter.py
@@ -14,11 +14,11 @@ json_dict = {
 'processed' : []
 }
 
-def exporter(method, *datasets):
+def exporter(directory, method, *datasets):
 	if method.lower() == 'json':
 		for data, json_list in zip(datasets, json_dict): # iterating over two two arrays at once
-			json_dict[json_list] = data # replace blank lists in json_dict with results
+			json_dict[json_list] = list(data) # replace blank lists in json_dict with results
 		json_string = json.dumps(json_dict, indent=4) # convert json_dict to a json styled string
-		savefile = open('exported.json', 'w+')
+		savefile = open('%s/exported.json' % directory, 'w+') # save
 		savefile.write(json_string)
 		savefile.close()
