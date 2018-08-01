79947b10c74948490d3393a543ad2019befefdf8
Connor Skees <39542938+ConnorSkees@users.noreply.github.com>
2018-08-01 06:13:36 -0400

    Updated changes variable

--- photon.py
+++ photon.py
@@ -76,7 +76,7 @@ args = parser.parse_args()
 
 def update():
     print('%s Checking for updates' % run)
-    changes = '''fixed a bug in link extracting regex;doesn't delete output directory if it exists''' # Changes must be seperated by ;
+    changes = '''added option to skip crawling of URLs that match a regex pattern;changed handling of seeds''' # Changes must be seperated by ;
     latest_commit = get('https://raw.githubusercontent.com/s0md3v/Photon/master/photon.py').text
 
     if changes not in latest_commit: # just a hack to see if a new version is available
