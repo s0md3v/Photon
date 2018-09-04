b183e97ca1e9ad5a7356c395305a7b78df80d326
Somdev Sangwan <s0md3v@gmail.com>
2018-09-04 14:36:25 +0530

    Add files via upload

---
+++ core/config.py
@@ -0,0 +1,2 @@
+intels = ['github.com', 'facebook.com', 'instagram.com', 'youtube.com']
+badTypes = ['png', 'jpg', 'jpeg', 'pdf', 'js', 'css', 'ico', 'bmp', 'svg', 'json', 'xml', 'xls', 'csv', 'docx']
