9f56d80bf022da72f22eb552a0306863c1108a04
theinfecteddrake <theinfecteddrake@gmail.com>
2018-09-17 01:15:01 +0100

    Ah finally resolved the crazyass error from there :D

--- photon.py
+++ photon.py
@@ -307,10 +307,8 @@ def zap(url):
         if matches: # if there are any matches
             print('%s URLs retrieved from sitemap.xml: %s' % (good, len(matches)))
             for match in matches:
-            	verb('Internal Link', match);internal.add(match)
-		
-         # would consistently give out indentation error why i don't know... drives me crazy... only solution i found :(
-
+                verb('Internal Link', match)
+                internal.add(match)
 ####
 # This functions checks whether a url matches a regular expression
 ####
