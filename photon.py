fd730f6de41ff0eff928976f4adc9e323c2c60e8
Somdev Sangwan <s0md3v@gmail.com>
2018-09-18 14:41:06 +0530

    Verbose output + animation fix for python2

--- photon.py
+++ photon.py
@@ -38,17 +38,16 @@ if machine.lower().startswith(('os', 'win', 'darwin', 'ios')):
 if not colors:
     end = red = white = green = yellow = run = bad = good = info = que = ''
 else:
-    end = '\033[0m'             # finish color
-    red = '\033[91m'            # red color
-    bold = '\033[1m'            # make text bold
-    white = '\033[1;97m'        # gray color
-    green = '\033[1;32m'        # green color
-    yellow = '\033[1;33m'       # yellow color
-    run = '\033[1;97m[~]'+end   # processing ...
-    bad = '\033[1;31m[-]'+end   # error or warning
-    good = '\033[1;32m[+]'+end  # good news
-    info = '\033[1;33m[!]'+end  # some info
-    que = '\033[1;34m[?]'+end   # user input
+    end = '\033[1;m'
+    red = '\033[91m'
+    white = '\033[1;97m'
+    green = '\033[1;32m'
+    yellow = '\033[1;33m'
+    run = '\033[1;97m[~]\033[1;m'
+    bad = '\033[1;31m[-]\033[1;m'
+    good = '\033[1;32m[+]\033[1;m'
+    info = '\033[1;33m[!]\033[1;m'
+    que = '\033[1;34m[?]\033[1;m'
 
 # Just a fancy ass banner
 print('''%s      ____  __          __
@@ -63,14 +62,15 @@ warnings.filterwarnings('ignore') # Disable SSL related warnings
 # Processing command line arguments
 parser = argparse.ArgumentParser()
 # Options
-parser.add_argument('-u', '--url', help='main url (eg. http://www.example.com)', dest='root')
-parser.add_argument('-c', '--cookie', help='cookie to be used', dest='cook')
+parser.add_argument('-u', '--url', help='root url', dest='root')
+parser.add_argument('-c', '--cookie', help='cookie', dest='cook')
 parser.add_argument('-r', '--regex', help='regex pattern', dest='regex')
-parser.add_argument('-e', '--export', help='export format type', dest='export')
+parser.add_argument('-e', '--export', help='export format', dest='export')
 parser.add_argument('-o', '--output', help='output directory', dest='output')
 parser.add_argument('-l', '--level', help='levels to crawl', dest='level', type=int)
 parser.add_argument('-t', '--threads', help='number of threads', dest='threads', type=int)
 parser.add_argument('-d', '--delay', help='delay between requests', dest='delay', type=float)
+parser.add_argument('-v', '--verbose', help='verbose output', dest='verbose', action='store_true')
 parser.add_argument('-s', '--seeds', help='additional seed urls', dest='seeds', nargs="+", default=[])
 
 parser.add_argument('--stdout', help='send variables to stdout', dest='std')
@@ -79,7 +79,6 @@ parser.add_argument('--exclude', help='exclude urls matching this regex', dest='
 parser.add_argument('--timeout', help='http request timeout', dest='timeout', type=float)
 
 # Switches
-parser.add_argument('-v', '--verbose', help='verbose output', dest='verbose', action='store_true')
 parser.add_argument('--dns', help='enumerate subdomains & dns data', dest='dns', action='store_true')
 parser.add_argument('--ninja', help='ninja mode', dest='ninja', action='store_true')
 parser.add_argument('--keys', help='find secret keys', dest='api', action='store_true')
@@ -94,7 +93,7 @@ args = parser.parse_args()
 
 def update():
     print('%s Checking for updates' % run)
-    changes = '''added the -v option; fixed 2 logic errors in detecting bad js scripts''' # Changes must be seperated by ;
+    changes = '''bug fixes;minor refactor;added --stdout option''' # Changes must be seperated by ;
     latest_commit = get('https://raw.githubusercontent.com/s0md3v/Photon/master/photon.py').text
 
     if changes not in latest_commit: # just a hack to see if a new version is available
@@ -118,15 +117,6 @@ def update():
     else:
         print('%s Photon is up to date!' % good)
 
-####
-# This function is for giving out a more verbose output
-####
-
-def verb(crawled_type, stuff): 
-    if args.verbose: # if '-v' is supplied
-        out = r'%s' % stuff # converting it to raw string
-        print('%s Found %s: %s%s%s' % (good, crawled_type, bold, stuff, end)) # print it out
-
 if args.update: # if the user has supplied --update argument
     update()
     quit() # quitting because files have been changed
@@ -139,6 +129,7 @@ else: # if the user hasn't supplied a url
     print('\n' + parser.format_help().lower())
     quit()
 
+verbose = args.verbose
 delay = args.delay or 0  # Delay between requests
 timeout = args.timeout or 6  # HTTP request timeout
 cook = args.cook or None  # Cookie
@@ -211,7 +202,6 @@ def requester(url):
     processed.add(url) # mark the url as crawled
     time.sleep(delay) # pause/sleep the program for specified time
     def normal(url):
-        # set headers for request
         headers = {
         'Host' : host, # ummm this is the hostname?
         'User-Agent' : random.choice(user_agents), # selecting a random user-agent
@@ -232,7 +222,7 @@ def requester(url):
         else:
             response.close()
             return 'dummy'
-    
+
     # developer.facebook.com API
     def facebook(url):
         return get('https://developers.facebook.com/tools/debug/echo/?q=' + url, verify=False).text
@@ -246,7 +236,6 @@ def requester(url):
 
     # codebeautify.org API
     def code_beautify(url):
-        # set headers and get request done
         headers = {
         'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
         'Accept' : 'text/plain, */*; q=0.01',
@@ -271,6 +260,14 @@ def requester(url):
         return normal(url)
 
 ####
+# This function gives verbose output
+####
+
+def verb(kind, string):
+    if verbose:
+        print ('%s %s: %s' % (info, kind, string))
+
+####
 # This function extracts links from .xml files
 ####
 
@@ -291,7 +288,7 @@ def zap(url):
             archived_urls = timeMachine(host, 'host')
         print ('%s Retrieved %i URLs from archive.org' % (good, len(archived_urls) - 1))
         for url in archived_urls:
-            verb('Link', url)
+            verb('Internal page', url)
             internal.add(url)
     response = get(url + '/robots.txt', verify=False).text # makes request to robots.txt
     if '<body' not in response: # making sure robots.txt isn't some fancy 404 page
@@ -301,7 +298,6 @@ def zap(url):
                 match = ''.join(match) # one item in match will always be empty so will combine both items
                 if '*' not in match: # if the url doesn't use a wildcard
                     url = main_url + match
-                    verb('Internal Link', url)
                     internal.add(url) # add the url to internal list for crawling
                     robots.add(url) # add the url to robots list
             print('%s URLs retrieved from robots.txt: %s' % (good, len(robots)))
@@ -311,8 +307,9 @@ def zap(url):
         if matches: # if there are any matches
             print('%s URLs retrieved from sitemap.xml: %s' % (good, len(matches)))
             for match in matches:
-                verb('Internal Link', match)
-                internal.add(match)
+                verb('Internal page', url)
+                internal.add(match) #cleaning up the url & adding it to the internal list for crawling
+
 ####
 # This functions checks whether a url matches a regular expression
 ####
@@ -349,12 +346,11 @@ def remove_regex(urls, regex):
 ####
 
 def is_link(url):
-    # file extensions don't need to be crawled
+    # file extension that don't need to be crawled and are files
     conclusion = False # whether the the url should be crawled or not
 
     if url not in processed: # if the url hasn't been crawled already
         if url.split('.')[-1].lower() in badTypes:
-            verb('File', url)
             files.add(url)
         else:
             return True # url can be crawled
@@ -369,7 +365,7 @@ def regxy(pattern, response):
     try:
         matches = findall(r'%s' % pattern, response)
         for match in matches:
-            verb('Link', match)
+            verb('Custom regex', match)
             custom.add(match)
     except:
         supress_regex = True
@@ -382,9 +378,8 @@ def intel_extractor(response):
     matches = findall(r'''([\w\.-]+s[\w\.-]+\.amazonaws\.com)|([\w\.-]+@[\w\.-]+\.[\.\w]+)''', response)
     if matches:
         for match in matches: # iterate over the matches
-            verb('Bad Intel',match)
-            bad_intel.add(match) # add it to bad intel list
-
+            verb('Intel', match)
+            bad_intel.add(match) # add it to intel list
 ####
 # This function extracts js files from the response body
 ####
@@ -392,20 +387,11 @@ def intel_extractor(response):
 def js_extractor(response):
     matches = findall(r'src=[\'"](.*?\.js)["\']', response) # extract .js files
     for match in matches: # iterate over the matches
-        # using first 2 'if' and 'elif' cases, the no of bad js scripts have most probably gone down to 0
-        if match.startswith('/'): # scripts which start with '/' aren't bad js 
-            js_script = main_url + match # add to main url
-            verb('JS Script', js_script)
-            scripts.add(js_script) # add them to scripts list
-        elif match.startswith('http'): # if js script link starts with 'http' it is a valid external js script and not bad js
-            verb('External JS Script', match)
-            scripts.add(match) # add them to scripts list
-        else: # else they might be bad scripts
-            verb('Bad JS Script', match)
-            bad_scripts.add(match) # add to bad intel scripts
+        verb('JS file', match)
+        bad_scripts.add(match)
 
 ####
-# This function calculates the entropy of a string to find whether its a key or not
+# This function calculates the entropy of a string
 ####
 
 def entropy(payload):
@@ -425,26 +411,26 @@ def extractor(url):
     matches = findall(r'<[aA].*href=["\']{0,1}(.*?)["\']', response)
     for link in matches: # iterate over the matches
         link = link.split('#')[0] # remove everything after a "#" to deal with in-page anchors
-        if is_link(link): 
+        if is_link(link): # checks if the urls should be crawled
             if link[:4] == 'http':
                 if link.startswith(main_url):
-                    verb('Internal Link', link)
+                    verb('Internal page', link)
                     internal.add(link)
                 else:
-                    verb('External Link', link)
+                    verb('External page', link)
                     external.add(link)
-            elif link[:2] == '//': 
+            elif link[:2] == '//':
                 if link.split('/')[2].startswith(host):
-                    verb('Internal Link', schema + link)
+                    verb('Internal page', link)
                     internal.add(schema + link)
                 else:
-                    verb('External Link', link)
+                    verb('External page', link)
                     external.add(link)
-            elif link[:1] == '/': # if url starts with '/' i.e. its an internal link
-                verb('Internal Link', main_url + link)
+            elif link[:1] == '/':
+                verb('Internal page', link)
                 internal.add(main_url + link)
             else:
-                verb('Internal Link', main_url + '/' + link)
+                verb('Internal page', link)
                 internal.add(main_url + '/' + link)
 
     if not only_urls:
@@ -453,10 +439,10 @@ def extractor(url):
     if args.regex and not supress_regex:
         regxy(args.regex, response)
     if api:
-        matches = findall(r'[\w-]{16,45}', response) # regex for finding keys
+        matches = findall(r'[\w-]{16,45}', response)
         for match in matches:
-            if entropy(match) >= 4: # if entropy score > 4 its a possible token or key
-                verb('Key', url + ': ' + match)
+            if entropy(match) >= 4:
+                verb('Key', match)
                 keys.add(url + ': ' + match)
 
 ####
@@ -469,7 +455,7 @@ def jscanner(url):
     for match in matches: # iterate over the matches, match is a tuple
         match = match[0] + match[1] # combining the items because one of them is always empty
         if not search(r'[}{><"\']', match) and not match == '/': # making sure it's not some js code
-            verb('JS Endpoint', match)
+            verb('JS enpoint', match)
             endpoints.add(match) # add it to the endpoints list
 
 ####
@@ -501,23 +487,18 @@ def flash(function, links): # This shit is NOT complicated, please enjoy
         for begin in range(0, len(links), thread_count): # range with step
             end = begin + thread_count
             splitted = links[begin:end]
-            threader(function, splitted) # start the multithreading
+            threader(function, splitted)
             progress = end
             if progress > len(links): # fix if overflow
                 progress = len(links)
-            if args.verbose:
-                print('\n\r%s Progress: %i/%i' % (info, progress, len(links)), end='\r') # prevent line overflow
-            else:
-                print('\r%s Progress: %i/%i' % (info, progress, len(links)), end='\r') # regular animate printing
+            print('\r%s Progress: %i/%i' % (info, progress, len(links)), end='\r')
+            sys.stdout.flush()
     else:
         threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
         futures = (threadpool.submit(function, link) for link in links)
         for i, _ in enumerate(concurrent.futures.as_completed(futures)):
             if i + 1 == len(links) or (i + 1) % thread_count == 0:
-                if args.verbose:
-                    print('\n%s Progress: %i/%i' % (info, i + 1, len(links)), end='\r') # to prevent line print overflow
-                else:
-                    print('%s Progress: %i/%i' % (info, i + 1, len(links)), end='\r') # normal print
+                print('%s Progress: %i/%i' % (info, i + 1, len(links)), end='\r')
     print('')
 
 then = time.time() # records the time at which crawling started
@@ -539,20 +520,17 @@ for level in range(crawl_level):
     print('%s Level %i: %i URLs' % (run, level + 1, len(links)))
     try:
         flash(extractor, links)
-    except KeyboardInterrupt: # if user breaks runtime
+    except KeyboardInterrupt:
         print('')
         break
 
 if not only_urls:
-    for match in bad_scripts: # check if script not in bad js scripts
+    for match in bad_scripts:
         if match.startswith(main_url):
-            verb('JS File', match)
-            scripts.add(match) # add js scripts
+            scripts.add(match)
         elif match.startswith('/') and not match.startswith('//'):
-            verb('JS File', main_url + match)
             scripts.add(main_url + match)
         elif not match.startswith('http') and not match.startswith('//'):
-            verb('JS File', main_url + '/' + match)
             scripts.add(main_url + '/' + match)
     # Step 3. Scan the JavaScript files for enpoints
     print('%s Crawling %i JavaScript files' % (run, len(scripts)))
@@ -560,19 +538,16 @@ if not only_urls:
 
     for url in internal:
         if '=' in url:
-            verb('Fuzzable Param', url)
-            fuzzable.add(url) # append to fuzzable parameters list
+            fuzzable.add(url)
 
     for match in bad_intel:
         for x in match: # because "match" is a tuple
             if x != '': # if the value isn't empty
-                verb('Intel', x)
-                intel.add(x) # append to fuzzable parameters list
+                intel.add(x)
         for url in external:
             try:
                 if tld.get_fld(url, fix_protocol=True) in intels:
-                    verb('Intel', url)
-                    intel.add(url) # add to intel list
+                    intel.add(url)
             except:
                 pass
 
@@ -583,7 +558,7 @@ def timer(diff):
     minutes, seconds = divmod(diff, 60) # Changes seconds into minutes and seconds
     try:
         time_per_request = diff / float(len(processed)) # Finds average time taken by requests
-    except ZeroDivisionError: # if float(len(processed)) == 0
+    except ZeroDivisionError:
         time_per_request = 0
     return minutes, seconds, time_per_request
 minutes, seconds, time_per_request = timer(diff)
@@ -592,25 +567,21 @@ minutes, seconds, time_per_request = timer(diff)
 if not os.path.exists(output_dir): # if the directory doesn't exist
     os.mkdir(output_dir) # create a new directory
 
-datasets = [files, intel, robots, custom, failed, internal, scripts, external, fuzzable, endpoints, keys] # list of all extracted types
-dataset_names = ['files', 'intel', 'robots', 'custom', 'failed', 'internal', 'scripts', 'external', 'fuzzable', 'endpoints', 'keys'] # as str
-
-####
-# This function writes the crawled links and files to different files
-####
+datasets = [files, intel, robots, custom, failed, internal, scripts, external, fuzzable, endpoints, keys]
+dataset_names = ['files', 'intel', 'robots', 'custom', 'failed', 'internal', 'scripts', 'external', 'fuzzable', 'endpoints', 'keys']
 
 def writer(datasets, dataset_names, output_dir):
     for dataset, dataset_name in zip(datasets, dataset_names):
         if dataset:
             filepath = output_dir + '/' + dataset_name + '.txt'
-            if python3: # if machine running python 3
-                with open(filepath, 'w+', encoding='utf8') as f: # make sure no encoding #errors
+            if python3:
+                with open(filepath, 'w+', encoding='utf8') as f:
                     f.write(str('\n'.join(dataset)))
                     f.write('\n')
-            else: # else if python2
+            else:
                 with open(filepath, 'w+') as f:
                     joined = '\n'.join(dataset)
-                    f.write(str(joined.encode('utf-8'))) # make sure no encoding errors
+                    f.write(str(joined.encode('utf-8')))
                     f.write('\n')
 
 writer(datasets, dataset_names, output_dir)
@@ -646,7 +617,7 @@ if args.export:
     # exporter(directory, format, datasets)
     exporter(output_dir, args.export, datasets)
 
-print('%s Results saved in %s%s%s/ directory' % (good, green, output_dir, end))
+print('%s Results saved in %s%s%s directory' % (good, green, output_dir, end))
 
 if args.std:
     for string in datasets[args.std]:
