7b24d5827d22168c25ad59b4ab1621c8c395b61c
Somdev Sangwan <s0md3v@gmail.com>
2019-01-30 12:45:12 +0530

    fixed subdomain enumeration

--- plugins/find_subdomains.py
+++ plugins/find_subdomains.py
@@ -8,10 +8,7 @@ def find_subdomains(domain):
     """Find subdomains according to the TLD."""
     result = set()
     response = get('https://findsubdomains.com/subdomains-of/' + domain).text
-    parts = response.split('data-row')
-    for part in parts:
-        matches = findall(
-            r'rel="nofollow" href="([^/]*)" target="_blank"', part)
-        for match in matches:
-            result.add(match)
+    matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
+    for match in matches:
+        result.add(match.replace(' ', '').replace('\n', ''))
     return list(result)
