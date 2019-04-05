84663427ffa57dfcfb286896787a9c3d75c2aa29
Somdev Sangwan <s0md3v@gmail.com>
2019-04-05 17:23:01 +0530

    Update flash.py

--- core/flash.py
+++ core/flash.py
@@ -3,26 +3,6 @@ import concurrent.futures
 
 from core.colors import info
 
-
-def threader(function, *urls):
-    """Start multiple threads for a function."""
-    threads = []
-    # Because URLs is a tuple
-    urls = urls[0]
-    # Iterating over URLs
-    for url in urls:
-        task = threading.Thread(target=function, args=(url,))
-        threads.append(task)
-    # Start threads
-    for thread in threads:
-        thread.start()
-    # Wait for all threads to complete their work
-    for thread in threads:
-        thread.join()
-    # Delete threads
-    del threads[:]
-
-
 def flash(function, links, thread_count):
     """Process the URLs and uses a threadpool to execute a function."""
     # Convert links (set) to list
